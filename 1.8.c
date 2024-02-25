722129f243ef87f782149792fe225681db3db8e1
nemo0110 <66428042+nemo0110@users.noreply.github.com>
2024-02-25 22:16:34 +0100

    Read a real number and a positive integer as input, calculate and print the power as output

--- 1.8.c
+++ 1.8.c
@@ -1,1 +1,28 @@
+/* Read a real number and a positive integer as input, calculate and print the power as output  */
+
+#include <stdio.h>
+
+float exponentiation(float base,int exponent){
+    float result = 1;
+    for(int i=0;i<exponent;i++){
+       result = result*base;
+    }
+    return result;
+}
+
+int main(){
+    int exponent;
+    float base;
+    printf("Enter a real number: ");
+    scanf("%f",&base);
+    printf("How many multiples of %f you want to print? ",base);
+    scanf("%d",&exponent);
+    while(exponent<0){
+        printf("Insert a positive integer number: ");
+        scanf("%d",&exponent);
+    }
+    printf("%f raised to the power of %d is: ",base,exponent);
+    printf("%f ",exponentiation(base,exponent));
+    return 0;
+}
 
