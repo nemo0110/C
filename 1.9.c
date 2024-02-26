5f681508671ee55380c906d9633a523fd3db1c42
nemo0110 <66428042+nemo0110@users.noreply.github.com>
2024-02-26 21:18:26 +0100

    Enter an integer > 1 and print the sequence 1,2,3...n,1,2,3... n-1,...,1,2,3,1,2,1

--- 1.9.c
+++ 1.9.c
@@ -1,1 +1,17 @@
+/* Enter an integer > 1 and print the sequence 1,2,3...n,1,2,3... n-1,...,1,2,3,1,2,1 */
 
+#include <stdio.h>
+
+int main(){
+    int n;
+    printf("Enter an integer greater than 1: ");
+    scanf("%d",&n);
+    for(n;n>0;n--){
+        for(int i=n;i>0;i--){
+            printf("%d ",i);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
